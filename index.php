8f600432575df186a74c4370d76a9576e7ecc907
tituswater <olowoyeyeoluwafemi93@yahoo.com>
2018-04-16 16:58:41 +0100

    Add existing file

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HNG INternship Task 3</title>
+	<link  rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+<div class="head">
+	<h1>Hi,</h1>
+	<h1>I am WATER from AndroidNGR, EKITI</h1>
+</div>
+
+<div class="compliment">
+	<p> a member of the HNG Internship 4.0 </p>
+</div>
+
+<div class="content">
+	<p class="header">DATE</p><br/>
+	<p class="d"><?php echo date('l F d, Y'); ?></p><br/>
+	<p class="header">TIME</p><br/>
+	<p class="t"><?php echo date('H : i : s');  ?></p>
+<br/><br/>
+
+	<div class="compliment">
+	<p> THERE IS NO PLACE LIKE 127.0.0.1 </p>
+</div>
+</div>
+
+</body>
+</html> 
